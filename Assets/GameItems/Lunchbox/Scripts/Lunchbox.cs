660e48018954933495125108f06af6515458b154
TobyDerrick <tobyderrick76@gmail.com>
2025-08-23 20:55:52 +0100

    [added] lunchbox class

---
+++ Assets/GameItems/Lunchbox/Scripts/Lunchbox.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lunchbox : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> foodInBox = new();
+
+    [SerializeField]
+    private FoodTraits foodTraitTotals;
+
+    public void AddItemToBox(GameObject itemToAdd)
+    {
+        foodInBox.Add(itemToAdd);
+
+        FoodScriptableObject itemSO = GameData.FoodItems.Get(itemToAdd.GetComponent<FoodItem>().id);
+        foodTraitTotals.AddTraitValues(itemSO.traits);
+    }
+
+    public void RemoveItemFromBox(GameObject itemToRemove)
+    {
+        foodInBox.Remove(itemToRemove);
+
+        FoodScriptableObject itemSO = GameData.FoodItems.Get(itemToRemove.GetComponent<FoodItem>().id);
+        foodTraitTotals.RemoveTraitValues(itemSO.traits);
+
+    }
+
+    public void ClearLunchBox()
+    {   
+        foodInBox.Clear();
+    }
+
+}
